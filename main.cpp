45505329d24dd02b6462d6bd3265c91df4b541cd
Victor-Aranda-Lopez <76120796+Victor-Aranda-Lopez@users.noreply.github.com>
2021-10-22 20:18:25 +0200

    Add files via upload
    Only Test mode.
    sensors: color, RGB led, moisture, light. Accel fails

--- main.cpp
+++ main.cpp
@@ -2,26 +2,31 @@
  * Copyright (c) 2019 ARM Limited
  * SPDX-License-Identifier: Apache-2.0
  */
-
+#include "TCS3472_I2C.h"
 #include "mbed.h"
-#include "TCS3472_I2C/TCS3472_I2C.h"
+#include "MMA8451Q.h"
 
 #define PERIOD_MEASUREMENT_TEST 2000000
 #define PERIOD_MEASUREMENT_NORMAL 30000000
-//I2C i2c(PA_10, PA_9); //pins for I2C communication (SDA, SCL)
-//Serial pc(USBTX, USBRX);
-int rgb_readings[4]; // declare a 4 element array to store RGB sensor readings
-//int sensor_addr = 41 << 1;
-enum Mode{ TEST, NORMAL, ADVANCED};
+#define MMA8451Q_SENSOR_ADDR	0x1d
 
-TCS3472_I2C rgb_sensor (PA_10, PA_9);
+int rgb_readings[4]; // declare a 4 element array to store RGB sensor readings
+
+enum Mode{TEST,NORMAL,ADVANCED};
+
+TCS3472_I2C rgb_sensor (PB_9, PB_8);
 
 DigitalOut TestMode_LED(LED1);
 DigitalOut NormalMode_LED(LED2);
 DigitalOut AdvancedMode_LED(LED3);
 InterruptIn button(PB_2);
-Ticker periodicMeasurement;
+AnalogIn moistureSensor(PA_0);
+AnalogIn lightSensor(PA_4);
+BusOut RGB_LED(PH_0,PH_1,PB_13);
+MMA8451Q accel_sensor(PB_9, PB_8,0x1d<<1);
 
+Ticker periodicMeasurement;
+char *dominant_color;
 bool read_sensors_flag;
 bool user_button_flag;
 void read_sensors()
@@ -32,6 +37,7 @@ void user_button()
 {
 		user_button_flag=true;
 }
+
 Mode mode = TEST;
 int main() {
 		//Initializarion
@@ -45,9 +51,9 @@ int main() {
 		button.mode(PullUp);
 		button.fall(&user_button);
 	
-		TestMode_LED    = 0;
-		NormalMode_LED  = 0;
-		AdvancedMode_LED= 0;
+		TestMode_LED     = 0;
+		NormalMode_LED   = 0;
+		AdvancedMode_LED = 0;
 		//Set high interrupt
 		/*rgb_sensor.enableInterrupt();
 		rgb_sensor.setHighInterruptThreshold(20000);
@@ -57,9 +63,36 @@ int main() {
 				read_sensors_flag=false;
 				//RGB sensor
 				rgb_sensor.getAllColors( rgb_readings ); // read the sensor to get red, green, and blue color data along with overall brightness
-
+				//Dominant color
+				if(rgb_readings[1]>rgb_readings[2] && rgb_readings[1]>=rgb_readings[3]){//If max=RED
+					RGB_LED=0b001;
+					dominant_color="RED";
+				}else if(rgb_readings[2]>rgb_readings[1] && rgb_readings[2]>rgb_readings[3]){//If max=Green
+					RGB_LED=0b010;
+					dominant_color="GREEN";
+				}else if(rgb_readings[3]>rgb_readings[1] && rgb_readings[3]>rgb_readings[2]){//If max=Blue
+					RGB_LED=0b100;
+					dominant_color="BLUE";					
+				}else{
+					RGB_LED=0b000;
+					dominant_color="NONE";
+				}
+				//Moisture sensor
+				uint16_t moisture_value = moistureSensor.read_u16();
+				float moisture_value_f= moisture_value*100.0/65536.0;
+				//Light sensor
+				uint16_t light_value = lightSensor.read_u16();
+				float light_value_f= light_value*100.0/65536.0;
+				//Acceleration sensor
+				float *accel_values= new float[3];
+				accel_sensor.getAccAllAxis(accel_values);
+				
 				//print values
-				printf("Colour: Clear=%d, Red=%d, Green=%d, Blue=%d\n",rgb_readings[0],rgb_readings[1],rgb_readings[2],rgb_readings[3]);
+				printf("Accelerometer: x_axis=%.2f, y_axis=%.2f, z_axis=%.2f\n",accel_values[0],accel_values[1],accel_values[2]);
+				printf("Light: %.1f%%\n",light_value_f);
+				printf("Moisture: %.1f%%\n",moisture_value_f);
+				printf("Color: Clear=%d, Red=%d, Green=%d, Blue=%d\n",rgb_readings[0],rgb_readings[1],rgb_readings[2],rgb_readings[3]);
+				printf("Dominant color: %s\n\n",dominant_color);
 			}
 			//Change mode
 			if(user_button_flag){
